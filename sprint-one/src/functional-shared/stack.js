20be0808e69a87e3b05a28d6220078d6ca00389e
Hack Reactor Students <github@hackreactor.com>
2013-09-25 16:12:55 -0700

    stack in the functional-shared pattern passes all tests

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,6 +1,28 @@
 var makeStack = function() {
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+  var instance = {
+    storage : {},
+    storageSize : 0
+  };
+  return _.extend(instance,stackMethods);
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push : function(value){
+    this.storage[this.storageSize] = value;
+    this.storageSize++;
+  },
 
+  pop : function(){
+    if (this.storageSize) {
+      this.storageSize--;
+      var temp = this.storage[this.storageSize];
+      delete this.storage[this.storageSize];
+      return temp;
+    }
+  },
+
+  size : function(){
+    return this.storageSize;
+  }
+
+};
