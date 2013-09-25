ec5b7b1a851575d15332b5f3f3032a6746d7e15e
Hack Reactor Students <github@hackreactor.com>
2013-09-25 13:28:22 -0700

    functional pattern stack tests pass

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,17 +3,28 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size; // Hint: set an initial value here
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
 
   instance.push = function(value){
+    size++;
+    storage[size] = value;
+    return value;
   };
 
   instance.pop = function(){
+    var temp = storage[size];
+    if (size) {
+      delete storage[size];
+      size--;
+      return temp;
+    }
+
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
