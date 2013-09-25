f3a8d44fe1544e5d77783a62ecf1e3242b823196
Hack Reactor Students <github@hackreactor.com>
2013-09-25 15:02:18 -0700

    queue in the functional pattern passes all tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var kicked = 0;
+  var total = 0;
 
   // Implement the methods below
 
   instance.enqueue = function(value){
+    storage[total] = value;
+    total ++;
   };
 
   instance.dequeue = function(){
+    if (instance.size()) {
+      var temp = storage[kicked];
+      delete storage[kicked];
+      kicked ++;
+      return temp;
+    }
   };
 
   instance.size = function(){
+    return total - kicked;
   };
 
   return instance;
