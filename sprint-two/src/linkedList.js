0204e550949e279f752902f92bb65e8e84a86ef6
Hack Reactor Students <github@hackreactor.com>
2013-09-26 14:30:27 -0700

    another linkedList checkpoint

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,35 +1,65 @@
 // Note: don't use an array to do this.
+
 var makeLinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
-  var total = 0;
-  var headIndex = 0;
 
   list.addToTail = function(value){
-    //create a new key:value pair in list, where the key is a total counter
-    if (arguments.length === 1){
-      list[total] = makeNode(value);
-      list.tail = list[total].value;
-      if (total === 0){
-        list.head = list[0].value;
+    if (arguments.length === 1) {
+      if (list.tail === null) {
+        list.tail = makeNode(value);
+        list.head = list.tail;
+      } else {
+        list.tail.next = makeNode(value);
+        list.tail = list.tail.next;
       }
-      total ++;
     }
   };
 
   list.removeHead = function(){
-    var temp = list.head;
-    delete list[headIndex];
-    headIndex ++;
-    list.head = list[headIndex].value;
+    if (list.head.next === null) {
+      list.tail = null;
+    }
+    var temp = list.head.value;
+    list.head = list.head.next;
     return temp;
   };
 
-  list.contains = function(){
-    //loop through the nodes inside list, searching for whether or not node.value === the target
-  };
+  list.contains = function(node,target){
+    if (node.value === target) {
+      return true;
+    } else if (node.next !== null) {
+      list.contains(node.next, target);
+    } else {
+      return false;
+    }
 
+/*
+      if (list.head !== null || list.tail != null){
+        
+      } esle{
+
+      }
+
+      if (list.head.value === target) {
+        return true;
+      } else{
+        list.head.next
+      }
+
+    }
+
+
+    if (list.head.value === target) {
+      return true;
+    } else if (list.head.next !== null) {
+      list.head.next.contains(target);
+    } else {
+      return false;
+    }
+  };
+*/
   return list;
 };
 
