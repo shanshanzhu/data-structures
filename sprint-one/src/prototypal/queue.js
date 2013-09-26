c36838b67e5d36497fc3fa8f349ae1112ad34ccc
Hack Reactor Students <github@hackreactor.com>
2013-09-25 17:42:07 -0700

    queue in the prototypal pattern passes all tests

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,6 +1,26 @@
-var makeQueue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+var makeQueue = function(){
+  var queue = Object.create(queueMethods);
+  queue.storage = {};
+  queue.kicked = 0;
+  queue.total = 0;
+  return queue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue : function(value){
+    this.storage[this.total] = value;
+    this.total ++;
+  },
+  dequeue : function(){
+    if (this.size()) {
+      var temp = this.storage[this.kicked];
+      delete this.storage[this.kicked];
+      this.kicked ++;
+      return temp;
+    }
+  },
+  size : function(){
+    return this.total - this.kicked;
+  }
+};
 
