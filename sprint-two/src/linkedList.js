2a1e029466335863cb6fb67bd7850a70c017b9a7
Hack Reactor Students <github@hackreactor.com>
2013-09-26 12:39:42 -0700

    checkpoint before linked list refactor

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,14 +3,31 @@ var makeLinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+  var total = 0;
+  var headIndex = 0;
 
-  list.addToTail = function(){
+  list.addToTail = function(value){
+    //create a new key:value pair in list, where the key is a total counter
+    if (arguments.length === 1){
+      list[total] = makeNode(value);
+      list.tail = list[total].value;
+      if (total === 0){
+        list.head = list[0].value;
+      }
+      total ++;
+    }
   };
 
   list.removeHead = function(){
+    var temp = list.head;
+    delete list[headIndex];
+    headIndex ++;
+    list.head = list[headIndex].value;
+    return temp;
   };
 
   list.contains = function(){
+    //loop through the nodes inside list, searching for whether or not node.value === the target
   };
 
   return list;
