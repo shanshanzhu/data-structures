e0b00d6a976442b8a6ae550ed92df503571ff5ee
Hack Reactor Students <github@hackreactor.com>
2013-09-27 16:30:29 -0700

    implemented the HashTable successfully; collisions resolved via linked list

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -12,23 +12,31 @@ var HashTable = function(){
 };
 
 HashTable.prototype.isLinkedList = function(obj) {
-  return obj.hasOwnProperty('head') && obj.hasOwnProperty('tail') && obj.hasOwnProperty('addtoTail') && obj.hasOwnProperty('removeHead') && obj.hasOwnProperty('contains');
+  return obj.hasOwnProperty('head') && obj.hasOwnProperty('tail') && obj.hasOwnProperty('addToTail') && obj.hasOwnProperty('removeHead') && obj.hasOwnProperty('contains');
+};
+HashTable.prototype.whatname = function (k) {
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(i);
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  console.log(i);
-  var existingVal = this._storage.get(i);
+  var bucket = this._storage.get(i);
   var linkedList;
 
-  if (existingVal !== undefined){
-    if (!this.isLinkedList(existingVal)) {
+  // if something already exists in storage under i
+  if (bucket !== undefined){
+    // first check that what exists is not a linked list, in which case we need to create one
+    if (!this.isLinkedList(bucket)) {
       linkedList = makeLinkedList();
+      linkedList.addToTail(bucket);
       linkedList.addToTail([k,v]);
       this._storage.set(i,linkedList);
+    // if what exists is a linked list, we add the [k, v] pair to the tail of the linked list
     } else {
-      linkedList.addToTail([k,v]);
+      bucket.addToTail([k,v]);
     }
+  // if nothing exists in storage under i, we set the value at i to be [k, v]
   } else {
     this._storage.set(i, [k,v]);
   }
@@ -37,11 +45,15 @@ HashTable.prototype.insert = function(k, v){
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(i);
+  // if what's in the bucket is a linked list, we need to search the linked list using the contains method for linked lists
   if (this.isLinkedList(bucket)) {
+    // we use the .contains method to search the linked list for k, and if we find k we return the corresponding v
+    // .contains returns an array containing [true/false, v]
     var result = bucket.contains(k);
     if (result[0]) {
       return result[1];
     }
+    // if we didn't find the key in the linked list, then k returns no value, i.e. undefined
     else {
       bucket = undefined;
     }
@@ -52,15 +64,27 @@ HashTable.prototype.retrieve = function(k){
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(i);
+  var result;
+  // if what's in the bucket is a linked list, we call the list.remove method
   if (this.isLinkedList(bucket)){
-    var result = bucket.conatins(k);
-    if (result[0]) {}
-  }
-  this._storage.each(function(value, key, collection){
-    if(i === key) {
-      collection.splice(key,1);
+    // if there are two items in our linked list, we need to convert the value in this._storage back
+    // to just one [k,v] pair if list.remove successfully removes something
+    if (bucket.length === 2){
+      result = bucket.remove(k);
+      if (result !== undefined) {
+        this._storage.set(i,bucket.head.value);
+      }
+      return result;
+    // if there are greater than two items in our linked list, we can just go ahead and look for k and remove it if we find it
+    } else {
+      return bucket.remove(k);
     }
-  });
+  //if what's in the bucket is not a linked list, we just return the value in [k,v] and reset this._storage[i] to undefined 
+  } else {
+    result = bucket[1];
+    this._storage.set(i, undefined);
+    return result;
+  }
 };
 
 // COPIED FROM hashTableHelpers.js
@@ -122,6 +146,7 @@ var makeLinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+  list.length = 0;
 
   list.addToTail = function(value){
     if (arguments.length === 1) {
@@ -132,6 +157,7 @@ var makeLinkedList = function(){
         list.tail.next = makeNode(value);
         list.tail = list.tail.next;
       }
+      list.length ++;
     }
   };
 
@@ -139,8 +165,9 @@ var makeLinkedList = function(){
     if (list.head.next === null) {
       list.tail = null;
     }
-    var temp = list.head.value;
+    var temp = list.head.value[1];
     list.head = list.head.next;
+    list.length --;
     return temp;
   };
 
@@ -148,31 +175,22 @@ var makeLinkedList = function(){
     if (node === undefined) {
       node = list.head;
     }
-/*  var nextNode = node.next
+
+    // we start by searching the head of the list
     if (list.head.value[0] === target) {
       return list.removeHead();
-    } else if (node === null) {
-      return undefinded;
-    } else if (node.next.value[0] === target) {
-      var temp = node.next.value[1];
-      node.next = node.next.next;
-        if (node.next === null) {
-          list.tail = node;
-        }
-      return temp;
-    } else {
-      return list.remove(target, node.next);
-    }
-*/
-    if (list.head.value[0] === target) {
-      list.removeHead();
+    // if we didn't find the target in the head node, we search the next node
     } else if (node.next.value[0] === target) { // this condition accommodates searching key, value array pairs in our hash table collision resolution process
+        var temp = node.next.value[1];
         node.next = node.next.next;
         if (node.next === null) {
           list.tail = node;
         }
+        list.length --;
+        return temp;
+    // if we didn't find the target in the next node, we search the node after that--if there is such a node
     } else if (node.next.next !== null) {
-      list.remove(target, node.next);
+        return list.remove(target, node.next);
     } else {
       return undefined;
     }
@@ -191,7 +209,6 @@ var makeLinkedList = function(){
       return [false];
     }
   };
-
 
   return list;
 };
