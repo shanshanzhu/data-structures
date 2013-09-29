ae0d3d02ef6b5dc6031b2ac71a6211e9aa9a4c5c
Hack Reactor Students <github@hackreactor.com>
2013-09-28 17:41:33 -0700

    .contain finished

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,2 +1,52 @@
-var makeBinarySearchTree = function(){
+var MakeBinarySearchTree = function(key, value){
+  this._left = null;
+  this._right = null;
+  this.key = key || null;
+  this.value = [value];
 };
+
+var _ = {};
+_.contains = function (list, target){
+  for (var i = 0; i < list.length; i ++) {
+    if (list[i] === target) {
+      return true;
+    }
+  }
+  return false;
+};
+
+
+MakeBinarySearchTree.prototype.insert = function (k, v) {
+  var tree = new MakeBinarySearchTree(k, v);
+  var temp;
+  if (this.key === null) {
+    this.value = tree.value;
+    this.key = tree.key;
+  }
+  else if (this.key === tree.key && (!_.contains(this.value, tree.value[0]) ) ) {
+    this.value.push(tree.value[0]);
+  }
+  else if (tree.key < this.key){
+    if (this._left !== null) {
+      this._left.insert (k,v);
+    } else {
+      this._left = tree;
+    }
+  }
+  else {
+    if (this._right !== null) {
+      this._right.insert(k,v);
+    } else {
+      this._right = tree;
+    }
+  }
+};
+
+MakeBinarySearchTree.prototype.contains = function (key) {
+  if (this.key === key){
+    return true;
+  } else if ()
+
+};
+
+
